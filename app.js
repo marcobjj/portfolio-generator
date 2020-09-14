94b1b7bbe49c2d6fd854cf916bc5c3f0bb1a4e2a
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-09-14 11:08:50 -0700

    adds filesystem

---
+++ app.js
@@ -0,0 +1,36 @@
+/*
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+
+    console.log('================');
+
+
+    profileDataArr.forEach(function(profileItem) {
+      console.log(profileItem);
+    });
+  };
+  
+  printProfileData(profileDataArgs);
+  */
+
+ const fs = require("fs");
+ const generatePage = require('./src/page-template.js');
+ 
+ const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+ const [name, github] = profileDataArgs;
+
+ 
+
+ console.log(generatePage(name,github));
+
+ fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
